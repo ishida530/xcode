2c5da95ec494d8005198e67dd23182bd3abc1282
ishida530 <ishida530@gmail.com>
2024-11-03 19:43:23 +0100

    clear form after send

--- frontend/src/app/component/currency-form/currency-form.component.ts
+++ frontend/src/app/component/currency-form/currency-form.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core'; 
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 import { CurrencyService, CurrencyRequest } from '../../service/currency.service';
 
 @Component({
@@ -21,17 +21,26 @@ export class CurrencyFormComponent {
   constructor(private currencyService: CurrencyService) { }
 
   getCurrencyValue() {
-    this.error = null; 
-    const request: CurrencyRequest = { currency: this.currency, name: this.name }; 
+    this.error = null;
+    const request: CurrencyRequest = { currency: this.currency, name: this.name };
     this.currencyService.getCurrencyValue(this.currency, this.name, request).subscribe(
       (response) => {
-        this.value = response.value; 
+        this.value = response.value;
         this.currencyValueFetched.emit();
+        this.clearFormFields();
+
+        setTimeout(() => {
+          this.value = null;
+        }, 3000);
       },
       (error) => {
         this.error = 'Error fetching currency value. Please try again.';
         console.error('Error:', error);
       }
     );
+  }
+  clearFormFields() {
+    this.currency = '';
+    this.name = '';
   }
 }
